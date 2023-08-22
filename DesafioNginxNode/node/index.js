49e3c3e44246d81021a306483aa07aa5a356570d
julia-leite <jlsouza@sga.pucminas.br>
2023-08-22 20:02:25 -0300

    nginx com nodejs

---
+++ DesafioNginxNode/node/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const faker = require('faker')
+
+const app = express();
+const port = 3000;
+
+const config = {
+  host: 'mysql',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb',
+}
+
+const mysql = require('mysql')
+
+const connection = mysql.createConnection(config)
+
+app.get('/', (req, res) => {
+  const name = faker.name.findName()
+
+  connection.query(`INSERT INTO people (nome) VALUES ('${name}')`)
+
+  connection.query(`SELECT nome FROM people`, (error, results, fields) => {
+    res.send(`
+      <h1>Full Cycle Rocks!</h1>
+      <ol>
+        ${!!results.length ? results.map(el => `<li>${el.nome}</li>`).join('') : ''}
+      </ol>
+    `)
+  })
+})
+
+app.listen(port, () => {
+  console.log('Up on:', port);
+})
